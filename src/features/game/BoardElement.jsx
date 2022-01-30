945233706f5845cfa9c5c1a5bf70b74ce890bb8c
Subhradeep <subhradeeppal30@gmail.com>
2022-01-30 19:34:55 +0530

    commit conflict solved

--- src/features/game/BoardElement.jsx
+++ src/features/game/BoardElement.jsx
@@ -4,8 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { setArray } from "./GameSlice";
 import ClearOutlinedIcon from "@mui/icons-material/ClearOutlined";
 import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined";
-const BoardElement = (props) => {
-
+export default function (props) {
   console.log(props);
   var classesOfBoard = [
     "posn top left",
@@ -72,54 +71,4 @@ const BoardElement = (props) => {
       </div>
     </>
   );
-
-	console.log(props.id);
-	const array = useSelector((state) => state.boardelements.elements);
-	const [classname, setClassname] = useState(props.cname);
-	useEffect(() => {
-		setClassname(props.cname);
-	}, []);
-
-	const dispatch = useDispatch();
-	return (
-		<>
-			<div
-				className={classname}
-				onClick={() => dispatch(setArray({ id: props.id, value: "X" }))}
-				onMouseEnter={() => {
-					if (!classname.includes(" hover_on_board_element")) {
-						setClassname(classname + " hover_on_board_element");
-					}
-				}}
-				onMouseLeave={() => {
-					let string = classname.split(" ");
-					string.pop();
-					setClassname(string.join(" "));
-				}}
-			>
-				<h1 className="boardSigns">
-					{array[props.id] == "X" ? (
-						<ClearOutlinedIcon
-							style={{
-								fontSize: "4rem",
-							}}
-						/>
-					) : (
-						""
-					)}
-					{array[props.id] == "O" ? (
-						<CircleOutlinedIcon
-							style={{
-								fontSize: "4rem",
-							}}
-						/>
-					) : (
-						""
-					)}
-				</h1>
-			</div>
-		</>
-
-};
-
-export default BoardElement;
+}
