320adbd3882889c28e74d98ecea1e8824150e2b3
ani <subhradeeppal30@gmail.com>
2022-02-02 01:14:10 +0530

    lower result name

--- src/features/game/Result.jsx
+++ src/features/game/Result.jsx
@@ -8,80 +8,80 @@ import { useSelector, useDispatch } from "react-redux";
 import { setOpponent } from "./ResultSlice";
 
 const Result = () => {
-  var username = localStorage.getItem("name");
-  var room = localStorage.getItem("room");
-  const [socketUrl, setSocketUrl] = useState(
-    "ws://127.0.0.1:8000/ws/seconduser/" + room + "/" + username + "/"
-  );
+	var username = localStorage.getItem("name");
+	var room = localStorage.getItem("room");
+	const [socketUrl, setSocketUrl] = useState(
+		"ws://127.0.0.1:8000/ws/seconduser/" + room + "/" + username + "/"
+	);
 
-  const [icon, setIcon] = useState(false);
-  const oppo = useSelector((state) => state.opponent.opponent);
-  // const [opponent, setOpponent] = useState(oppo);
-  const dispatch = useDispatch();
-  const [messageHistory, setMessageHistory] = useState([]);
-  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
-  useEffect(() => {
-    if (lastMessage !== null) {
-      setMessageHistory((prev) => prev.concat(lastMessage));
-      var bothClient = JSON.parse(lastMessage.data);
-      if (localStorage.getItem("name") === bothClient.first_client) {
-        // setOpponent(bothClient.second_client);
-        dispatch(setOpponent(bothClient.second_client));
-        setIcon(true);
-        console.log(bothClient);
-      }
-      if (localStorage.getItem("name") === bothClient.second_client) {
-        setOpponent(bothClient.first_client);
-        dispatch(setOpponent(bothClient.first_client));
-        setIcon(true);
-      }
-      if (bothClient.second_client == false) {
-        setOpponent("Opps !");
-        dispatch(setOpponent("Oops !"));
-        setIcon(false);
-      }
-    }
-  }, [lastMessage, setMessageHistory]);
-  return (
-    <>
-      <div className="ownProfile">
-        <Fab>
-          <AccountCircleIcon
-            fontSize="large"
-            style={{
-              color: "#3B3B98",
-            }}
-          />
-        </Fab>
-        <h2>You</h2>
-      </div>
-      <div className="volumeBar">
-        <Volume />
-      </div>
-      <div className="bothPlayer" style={{ backgroundColor: "primary" }}>
-        <p className="room">Room</p>
-        <p className="roomcode">adi8u39</p>
-        <div className="players">
-          <div className="player1">
-            <Fab>
-              <AccountCircleIcon fontSize="large" />
-            </Fab>
-            <p>You</p>
-          </div>
-          <p className="vs">Vs</p>
-          <div className="player2">
-            <Fab>
-              {icon ? (
-                <AccountCircleIcon fontSize="large" />
-              ) : (
-                <HelpOutlineIcon fontSize="large" color="error" />
-              )}
-            </Fab>
-            <p>{oppo.toUpperCase()}</p>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	const [icon, setIcon] = useState(false);
+	const oppo = useSelector((state) => state.opponent.opponent);
+	// const [opponent, setOpponent] = useState(oppo);
+	const dispatch = useDispatch();
+	const [messageHistory, setMessageHistory] = useState([]);
+	const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+	useEffect(() => {
+		if (lastMessage !== null) {
+			setMessageHistory((prev) => prev.concat(lastMessage));
+			var bothClient = JSON.parse(lastMessage.data);
+			if (localStorage.getItem("name") === bothClient.first_client) {
+				// setOpponent(bothClient.second_client);
+				dispatch(setOpponent(bothClient.second_client));
+				setIcon(true);
+				console.log(bothClient);
+			}
+			if (localStorage.getItem("name") === bothClient.second_client) {
+				setOpponent(bothClient.first_client);
+				dispatch(setOpponent(bothClient.first_client));
+				setIcon(true);
+			}
+			if (bothClient.second_client == false) {
+				setOpponent("Opps !");
+				dispatch(setOpponent("Disconnected"));
+				setIcon(false);
+			}
+		}
+	}, [lastMessage, setMessageHistory]);
+	return (
+		<>
+			<div className="ownProfile">
+				<Fab>
+					<AccountCircleIcon
+						fontSize="large"
+						style={{
+							color: "#3B3B98",
+						}}
+					/>
+				</Fab>
+				<h2>You</h2>
+			</div>
+			<div className="volumeBar">
+				<Volume />
+			</div>
+			<div className="bothPlayer" style={{ backgroundColor: "primary" }}>
+				<p className="room">Room</p>
+				<p className="roomcode">adi8u39</p>
+				<div className="players">
+					<div className="player1">
+						<Fab>
+							<AccountCircleIcon fontSize="large" />
+						</Fab>
+						<p>You</p>
+					</div>
+					<p className="vs">Vs</p>
+					<div className="player2">
+						<Fab>
+							{icon ? (
+								<AccountCircleIcon fontSize="large" />
+							) : (
+								<HelpOutlineIcon fontSize="large" color="error" />
+							)}
+						</Fab>
+						<p>{oppo.toUpperCase()}</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 export default Result;
